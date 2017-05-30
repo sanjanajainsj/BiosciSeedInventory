fc57053481dec323435e2a0d0e47ee72b2eeb6af
sanjanajainsj <sanjanajain200gmail.com>
2017-05-30 12:40:27 +0700

    keep trying

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
     end
   end
   get 'site/index'
+  root :to => "inventories#index"
   #get 'inventories/search' => 'inventories#search'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
